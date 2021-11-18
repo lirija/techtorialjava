2be2b97a9610a46d03e4b97a464a8a3c65c0ed06
lirijalalic <lirijalalic76@gmail.com>
2021-11-18 13:38:51 -0600

    First Java Commit

---
+++ src/GIveANynameyouwant.java
@@ -0,0 +1,5 @@
+public class GIveANynameyouwant {
+    public static void main(String[] args) {
+        // Do all your implementation here
+    }
+}
---
+++ src/Primitives/PrimitivesPractice.java
@@ -0,0 +1,21 @@
+package Primitives;
+
+public class PrimitivesPractice {
+
+    public static void main(String[] args) {
+
+       // addition -----+
+        int num1 = 3;
+        int num2 = 4;
+        System.out.println(num1=num2);
+        System.out.println(new StringBuilder().append("Tool is: ").append(num1 + num2).toString());
+
+        double difference=num2 -num1;
+        double product = difference * num2;
+        System.out.println(product);
+
+       //  int remainder1 = a % b;
+
+
+
+    }}
